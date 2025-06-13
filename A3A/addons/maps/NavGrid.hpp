10603ffabd26676dc48e0ee626cda1b45cd4f387
stutpip123 <77023662+stutpip123@users.noreply.github.com>
2025-06-13 14:30:43 +0100

    Regero

--- A3A/addons/maps/NavGrid.hpp
+++ A3A/addons/maps/NavGrid.hpp
@@ -59,4 +59,5 @@ class NavGrid {
 	stubbhult = QPATHTOFOLDER(Antistasi_stubbhult.stubbhult\navGrid.sqf);
 	kunduz_valley = QPATHTOFOLDER(Antistasi_kunduz_valley.kunduz_valley\navGrid.sqf);
 	isladuala3 = QPATHTOFOLDER(Antistasi_isladuala3.isladuala3\navGrid.sqf);
+	regero = QPATHTOFOLDER(Antistasi_regero.regero\navGrid.sqf);
 };
