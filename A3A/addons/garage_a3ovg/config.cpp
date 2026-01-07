1da008bea9b89ba13b39c042d80a7132ac3823e0
Oliver Schieche <post@oliver-schieche.de>
2026-01-07 22:02:21 +0100

    New addon for A3OVG

---
+++ A3A/addons/garage_a3ovg/config.cpp
@@ -0,0 +1,18 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"A3A_core", "A3OVG_api"};
+        skipWhenMissingDependencies = 1;
+        author = AUTHOR;
+        authors[] = { AUTHORS };
+        authorUrl = "";
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgEventHandlers.hpp"
---
+++ A3A/addons/garage_a3ovg/script_component.hpp
@@ -0,0 +1,2 @@
+#define COMPONENT garage_a3ovg
+#include "\x\A3A\addons\core\Includes\script_mod.hpp"
