6fec4d59884d9399b70a8993fa5dd8e71d0f00f8
Oliver Schieche <post@oliver-schieche.de>
2026-01-31 17:59:27 +0100

    Introduce temporary resting place for hardcoded variables

---
+++ A3A/addons/core/functions/init/varsHardcoded.hpp
@@ -0,0 +1,17 @@
+/* ----------------------------------------------------------------------------
+File: A3A/addons/core/functions/init/varsHardcoded.hpp
+
+Description:
+    Hopefully temporary resting place for hardcoded variables until they become
+    parameters.
+
+Environment:
+    Client/Server
+
+Author:
+    UnseenKill/gor3Splatter
+---------------------------------------------------------------------------- */
+
+// When getting marker sizes for zero-size markers (emplacements), pretend
+// they're really this big.
+GVAR(zeroSizeMarkerBlowup) = 50;
