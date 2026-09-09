22c942dcc7ebc3ed9615d1b57ff8f23d7d56eb21
Oliver Schieche <post@oliver-schieche.de>
2026-09-09 16:13:41 +0200

    Update for new plugin architecture

--- A3A/addons/ultimate/plugins/heatmap/script_component.hpp
+++ A3A/addons/ultimate/plugins/heatmap/script_component.hpp
@@ -1,6 +1,6 @@
-#define COMPONENT ultimate
-#define SUBCOMPONENT addon_heatmap
-#include "\x\A3A\addons\core\Includes\script_mod.hpp"
+#define FUNCTION_NAME_INSERT fn
+#define SUBCOMPONENT heatmap
+#include "..\plugin.hpp"
 #include "\x\A3A\addons\ultimate\script_macros_oo.hpp"
 
 #define HEATMAP_CLASS DOUBLES(SUBADDON,Heatmap)
